6182dcfdc7d371a00883e11e6a8ab325c3742b34
ubaeida <ubaeida.alkayal@gmail.com>
2022-10-25 11:56:00 +0300

    fixing docker compose errors

--- TicketApplication/app.py
+++ TicketApplication/app.py
@@ -21,14 +21,20 @@ InfoController()
 rest_port = 8050
 localhost = 'localhost'
 eureka_client.init(eureka_server=os.environ.get('eureka_server'),
-                   app_name="ticket-application-server",
+                   app_name= os.environ.get('app_name'),
                    instance_port=rest_port,
-                   instance_host=localhost
+                   # instance_host=os.environ.get('instance_host')
                    )
-# os.environ['eureka_server'] = "http://eureka-server:8761"
+
+# eureka_client.init(eureka_server="http://localhost:8761/eureka/",
+#                    app_name="ticket-application-server",
+#                    instance_port=rest_port,
+#                    instance_host=localhost
+#                    )
+
 if __name__ == '__main__':
     db.create_all()
-    app.run(debug=True, host='localhost', port=8050)
+    app.run(debug=True, host='0.0.0.0', port=8050)
 else:
     # Creates the database and tables
     # for the production environment
